32c4d6e25e5adda235aa3c46a503ab163ded2193
pedro_francoso <pedro.francoso@wr.sc.usp.br>
2024-05-04 10:26:08 -0300

    first commit

---
+++ hashing.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hashing.h"
+
+
+// Função de hash -> valores ASCII das primeiras duas letras da palavra e duas ultimas letras da palavra
+int hash_function(char *word) {
+    int hash = 0;
+
+    //Duas primeiras letras
+    hash |= word[0];
+    hash |= word[1] << 8;
+
+    //Duas últimas letras
+    int len = strlen(word);
+    hash |= word[len - 2] << 16;
+    hash |= word[len - 1] << 24;
+
+    return hash % TAMANHO_TABELA;
+}
+
+void inicializa_tabela(Tabela *tabela, char *word){
+    int index = hash_function(word);
+
+    Node *new_node = malloc(sizeof(Node));
+    if (new_node == NULL){
+        fprintf(stderr, "Erro ao alocar memória para nó.");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->word = strdup(word);
+    new_node->next = NULL;
+
+    if(tabela->table[index] == NULL){
+        tabela->table[index] = new_node;
+    } else {
+        Node *current = tabela->table[index];
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+    return 0;
+}
+
+// Função de busca
+int busca_tabela(Tabela *tabela, char *word){
+    int index = hash_function(word);
+
+    Node *current = tabela->table[index];
+    while (current != NULL) {
+        if (strcmp(current->word, word) == 0) {
+            return 1; //Encontrado
+        }
+        current = current->next;
+    }
+    return 0; //Não encontrado
+}
+
+void liberar_tabela(Tabela *tabela) {
+    for (int i = 0; i < TAMANHO_TABELA; i++){
+        Node *current = tabela->table[i];
+        while (current != NULL) {
+            Node *temp = current;
+            current = current->next;
+            free(temp->word);
+            free(temp);
+        }        
+    }
+}
---
+++ hashing.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAMANHO_TABELA 100
+
+
+// Definição dos nós da tabela
+typedef struct {
+    char *word;
+    struct Node *next;
+} Node;
+
+// Definição das tabelas (de simbolos e palavras reservadas e de simbolos e palavras)
+typedef struct {
+    Node *table[TAMANHO_TABELA]
+} Tabela;
+
+
+int hash_function(char *word);
+
+void inicializa_tabela(Tabela *tabela, char *word);
+
+int busca_tabela(Tabela *tabela, char *word);
+
+void liberar_tabela(Tabela *tabela);
