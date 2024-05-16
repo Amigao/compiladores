6a8191295fd7b3a75b88e61423fd513884aedecf
pedro_francoso <pedro.francoso@wr.sc.usp.br>
2024-05-16 01:31:46 -0300

    feat: agora o codigo ignora comentarios, mas a main ficou feia

--- main.c
+++ main.c
@@ -43,7 +43,20 @@ int main(int argc, char *argv[]) {
         }
 
         if (isspace(c) || c == '\n') {
-            if(tok.final){
+            if(tok.state == 10){
+                if (c == '\n'){
+                    printf("COMENTARIO IGNORADO; %s", buffer);
+                    current_state = INITIAL_STATE;
+                    i = 0;
+                    buffer[i] = '\0';
+                    tok.final = false;
+                } else {                    
+                    buffer[i] = c;
+                    buffer[i + 1] = '\0';
+                    i++;
+                }
+            }
+            else if(tok.final){
                 fprintf(output_file, "%s, %s\n", tok.token, tok.identifier);
                 current_state = INITIAL_STATE;
                 i = 0;
